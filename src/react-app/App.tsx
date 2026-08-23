817aeeaafd2efea15e338114656daaec9282403f
danielcepatterson <danielcepatterson@gmail.com>
2026-08-23 00:02:48 -0400

    Add additional verified Jerry Garcia photos to the collection

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -21,6 +21,38 @@ const JERRY_PHOTOS = [
 		src: "https://upload.wikimedia.org/wikipedia/commons/2/24/Jerry_Garcia%27s_Lightning_Bolt_guitar.jpg",
 		caption: "Jerry Garcia's iconic Lightning Bolt guitar",
 	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/6/6b/Jerry_Garcia_by_Jay_Blakesberg.jpg",
+		caption: "Jerry Garcia — photo by Jay Blakesberg",
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/0/04/Grateful_Dead_-_Jerry_Garcia_%28cropped%29.jpg",
+		caption: "Jerry Garcia — Grateful Dead (cropped)",
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Jerry_Garcia_at_Red_Rocks_taken_1987-08-11.jpg",
+		caption: "Jerry Garcia at Red Rocks, August 11, 1987",
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Jerry_Garcia%27s_Rosebud.jpg",
+		caption: "Jerry Garcia's 'Rosebud' guitar",
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/9/92/Detail_of_Jerry_Garcia_%27Rosebud%27_Guitar_-_Rock_%26_Roll_Hall_of_Fame_and_Museum_-_Cleveland_-_Ohio_-_USA_%2815304059750%29.jpg",
+		caption: "Jerry Garcia's 'Rosebud' guitar detail — Rock & Roll Hall of Fame",
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Detail_of_Jerry_Garcia_Top_Hat_Guitar_-_Rock_%26_Roll_Hall_of_Fame_and_Museum_-_Cleveland_-_Ohio_-_USA_%2815304055920%29.jpg",
+		caption: "Jerry Garcia's 'Top Hat' guitar detail — Rock & Roll Hall of Fame",
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Jerry_Garcia%27s_%22Wolf%22_Guitar_%281973%2C_serial_no._D._Irwin_001%29_by_Doug_Irwin_-_Play_It_Loud._MET_%282019-05-13_19.36.59_by_Eden%2C_Janine_and_Jim%29.jpg",
+		caption: "Jerry Garcia's 'Wolf' guitar (1973) — MET Museum",
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Edgefield_Jerry_Garcia_statue.jpg",
+		caption: "Jerry Garcia statue at Edgefield",
+	},
 ];
 
 function getRandomPhoto() {
